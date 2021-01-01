0478a0fdd026d61701fddb2230007f8d39f20ce9
yuanyuanwa <1820001685@qq.com>
2021-01-01 16:54:11 +0800

    save

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -1,46 +1,190 @@
 <template>
-    <div class="Pos">
-      <!-- elment里的所有组件都是以el开头的 -->
-      <el-row>
-        <el-col :span='7' class="pos-order" id="order-list">
+  <div class="Pos">
+    <!-- elment里的所有组件都是以el开头的 -->
+    <el-row>
+      <el-col :span='7' class="pos-order" id="order-list">
+        <el-tabs>
+          <el-tab-pane label='点餐'>
+            <el-table :data='tableData' border style="width: 100%;">
+              <el-table-column prop='goodsName' label='商品名称'></el-table-column>
+              <el-table-column prop='count' label='数量' width='80'></el-table-column>
+              <el-table-column prop='price' label='金额' width='80'></el-table-column>
+              <el-table-column label='操作' width='80' fixed='right'>
+                <template v-slot='scope'>
+                  <el-button type='text' size='small'>删除</el-button>
+                  <el-button type='text' size='small'>增加</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+            <div class="div-btn">
+              <el-button type='warning'>挂单</el-button>
+              <el-button type='danger'>删除</el-button>
+              <el-button type='success'>结账</el-button>
+            </div>
+          </el-tab-pane>
+          <el-tab-pane label='挂单'>
+            挂单dan
+          </el-tab-pane>
+          <el-tab-pane label='外卖'>
+            外卖
+          </el-tab-pane>
+        </el-tabs>
+      </el-col>
+
+      <el-col :span='17'>
+        <div class="often-goods">
+          <div class="title">常用商品</div>
+          <div class="often-goods-list">
+            <ul>
+              <li v-for='goods in oftenGoods'>
+                <span>{{goods.goodsName}}</span>
+                <span class="o-price">￥{{goods.price}}元</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="goods-type">
           <el-tabs>
-            <el-tab-pane label='点餐'>
-              <el-table>
-                <el-table-column prop='goodsName' label='商品名称'></el-table-column>
-                <el-table-column prop='count' label='数量'width='80'></el-table-column>
-                <el-table-column prop='price' label='金额'width='80'></el-table-column>
-                <el-table-column label='操作' width='80'></el-table-column>
-              </el-table>
+            <el-tab-pane label='汉堡'>
+              <div>
+                <ul class='cookList'>
+                  <li v-for='goods in type0Goods'>
+                    <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
+                    <span class="foodName">{{goods.goodsName}}</span>
+                    <span class="foodPrice">￥{{goods.price}}元</span>
+                  </li>
+                </ul>
+              </div>
             </el-tab-pane>
-            <el-tab-pane label='挂单'>
-              挂单dan
+            <el-tab-pane label='小食'>
+              小食
             </el-tab-pane>
-            <el-tab-pane label='外卖'>
-              外卖
+            <el-tab-pane label='饮料'>
+              饮料
             </el-tab-pane>
+            <el-tab-pane label='套餐'>
+              套餐
+            </el-tab-pane>
+
           </el-tabs>
-        </el-col>
-        <el-col>
-          我是产品栏
-        </el-col>
-      </el-row>
-    </div>
-  </template>
-  <script>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+  import aaa from '../../assets/111.jpg'
   export default {
     name: 'Pos',
+    data() {
+      return {
+        tableData: [{
+          goodsName: '可口可乐',
+          price: 8,
+          count: 1
+        }, {
+          goodsName: '香辣鸡腿堡',
+          price: 15,
+          count: 1
+        }, {
+          goodsName: '爱心薯条',
+          price: 8,
+          count: 1
+        }, {
+          goodsName: '甜筒',
+          price: 8,
+          count: 1
+        }
+        ],
+        oftenGoods: [
+         
+        ],
+        type0Goods: [
+
+        ],
+      }
+    },
     // elment是虚拟的，所以要在所有虚拟dom加载完成之后才在钩子函数里写这个，所以选择mounted
-    mounted:function(){
-        var orderHeight=document.body.clientHeight;
-        // console.log(orderHeight);
-        document.getElementById('order-list').style.height=orderHeight+'px'
+    mounted: function () {
+      var orderHeight = document.body.clientHeight;
+      // console.log(orderHeight);
+      document.getElementById('order-list').style.height = orderHeight + 'px'
     }
   }
-  </script>
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-    .pos-order{
-      background-color: #F9FAFC;
-      border-right: 1px solid #C0CCDA;
-    }
-  </style>
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .pos-order {
+    background-color: #F9FAFC;
+    border-right: 1px solid #C0CCDA;
+  }
+  /deep/ .el-tabs__nav-wrap {
+    background-color: #fff;
+  }
+  .div-btn {
+    margin-top: 15px;
+  }
+
+  .title {
+    height: 20px;
+    border-bottom: 1px solid #D3DCE6;
+    background-color: #F9FAFC;
+    padding: 10px;
+    text-align: left;
+  }
+
+  .often-goods-list ul li {
+    list-style: none;
+    float: left;
+    border: 1px solid #E5E9F2;
+    padding: 10px;
+    margin: 5px;
+    background-color: #fff;
+  }
+
+  .o-price {
+    color: #58B7FF;
+  }
+
+  .goods-type {
+    clear: both;
+
+  }
+ 
+  .cookList li {
+    list-style: none;
+    width: 23%;
+    border: 1px solid #E5E9F2;
+    height: auot;
+    overflow: hidden;
+    background-color: #F9FAFC;
+    padding: 2px;
+    float: left;
+    margin: 2px;
+
+  }
+
+  .cookList li span {
+
+    display: block;
+    float: left;
+  }
+
+  .foodImg {
+    width: 40%;
+  }
+
+  .foodName {
+    font-size: 18px;
+    padding-left: 10px;
+    color: brown;
+
+  }
+
+  .foodPrice {
+    font-size: 16px;
+    padding-left: 10px;
+    padding-top: 10px;
+  }
+</style>
